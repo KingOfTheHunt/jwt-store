cc3550600330884fe2258d5afa48fa41d4cc46f0
Davi <davifrancisco214@outlook.com>
2024-02-17 07:27:56 -0300

    Entity e ValueObject

---
+++ JwtStore.Core/SharedContext/Entities/Entity.cs
@@ -0,0 +1,12 @@
+namespace JwtStore.Core.SharedContext.Entities;
+
+public abstract class Entity : IEquatable<Guid>
+{
+    public Guid Id { get; }
+
+    protected Entity() =>
+        Id = Guid.NewGuid();
+
+    public bool Equals(Guid id) => 
+        Id == id;
+}
---
+++ JwtStore.Core/SharedContext/ValueObjects/ValueObject.cs
@@ -0,0 +1,5 @@
+namespace JwtStore.Core.SharedContext.ValueObjects;
+
+public abstract class ValueObject
+{
+}
