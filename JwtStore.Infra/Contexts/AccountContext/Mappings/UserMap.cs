f2ff9e4fe24aa7f72a10206fbcb611ebc87bf82a
Davi <davifrancisco214@outlook.com>
2024-02-19 07:27:40 -0300

    Criando o DataContext

--- JwtStore.Infra/Contexts/AccountContext/Mappings/UserMap.cs
+++ JwtStore.Infra/Contexts/AccountContext/Mappings/UserMap.cs
@@ -2,78 +2,77 @@ using JwtStore.Core.Contexts.AccountContext.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace JwtStore.Infra.Contexts.AccountContext.Mappings
+namespace JwtStore.Infra.Contexts.AccountContext.Mappings;
+
+public class UserMap : IEntityTypeConfiguration<User>
 {
-    public class UserMap : IEntityTypeConfiguration<User>
+    public void Configure(EntityTypeBuilder<User> builder)
     {
-        public void Configure(EntityTypeBuilder<User> builder)
-        {
-            builder.ToTable("User");
+        builder.ToTable("User");
 
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id)
-                .IsRequired()
-                .UseIdentityColumn()
-                .ValueGeneratedOnAdd();
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .IsRequired()
+            .UseIdentityColumn()
+            .ValueGeneratedOnAdd();
 
-            builder.Property(x => x.Name)
-                .IsRequired()
-                .HasColumnName("Name")
-                .HasColumnType("NVARCHAR")
-                .HasMaxLength(120);
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(120);
 
-            builder.Property(x => x.Image)
-                .IsRequired()
-                .HasColumnName("Image")
-                .HasColumnType("VARCHAR")
-                .HasMaxLength(255);
-            
-            // Mapeando os value object
-            builder.OwnsOne(x => x.Email)
-                .Property(y => y.Address)
-                .IsRequired()
-                .HasColumnName("Email")
-                .HasColumnType("VARCHAR")
-                .HasMaxLength(150);
-            
-            builder.OwnsOne(x => x.Email)
-                .OwnsOne(y => y.Verification)
-                .Property(z => z.Code)
-                .IsRequired()
-                .HasColumnName("EmailVerificationCode")
-                .HasColumnType("VARCHAR")
-                .HasMaxLength(6);
+        builder.Property(x => x.Image)
+            .IsRequired()
+            .HasColumnName("Image")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(255);
+        
+        // Mapeando os value object
+        builder.OwnsOne(x => x.Email)
+            .Property(y => y.Address)
+            .IsRequired()
+            .HasColumnName("Email")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(150);
+        
+        builder.OwnsOne(x => x.Email)
+            .OwnsOne(y => y.Verification)
+            .Property(z => z.Code)
+            .IsRequired()
+            .HasColumnName("EmailVerificationCode")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(6);
 
-            builder.OwnsOne(x => x.Email)
-                .OwnsOne(y => y.Verification)
-                .Property(z => z.ExpiresAt)
-                .IsRequired(false)
-                .HasColumnName("EmailVerificationExpiresAt")
-                .HasColumnType("TIME");
+        builder.OwnsOne(x => x.Email)
+            .OwnsOne(y => y.Verification)
+            .Property(z => z.ExpiresAt)
+            .IsRequired(false)
+            .HasColumnName("EmailVerificationExpiresAt")
+            .HasColumnType("TIME");
 
-            builder.OwnsOne(x => x.Email)
-                .OwnsOne(y => y.Verification)
-                .Property(z => z.VerifiedAt)
-                .IsRequired(false)
-                .HasColumnName("EmailVerificationVerifiedAt")
-                .HasColumnType("TIME");
+        builder.OwnsOne(x => x.Email)
+            .OwnsOne(y => y.Verification)
+            .Property(z => z.VerifiedAt)
+            .IsRequired(false)
+            .HasColumnName("EmailVerificationVerifiedAt")
+            .HasColumnType("TIME");
 
-            builder.OwnsOne(x => x.Email)
-                .OwnsOne(y => y.Verification)
-                .Ignore(z => z.IsActive);
+        builder.OwnsOne(x => x.Email)
+            .OwnsOne(y => y.Verification)
+            .Ignore(z => z.IsActive);
 
-            builder.OwnsOne(x => x.Password)
-                .Property(y => y.Hash)
-                .IsRequired()
-                .HasColumnName("PasswordHash")
-                .HasColumnType("NVARCHAR");
+        builder.OwnsOne(x => x.Password)
+            .Property(y => y.Hash)
+            .IsRequired()
+            .HasColumnName("PasswordHash")
+            .HasColumnType("NVARCHAR");
 
-            builder.OwnsOne(x => x.Password)
-                .Property(y => y.ResetCode)
-                .IsRequired()
-                .HasColumnName("PasswordRestCode")
-                .HasColumnType("VARCHAR")
-                .HasMaxLength(8);
-        }
+        builder.OwnsOne(x => x.Password)
+            .Property(y => y.ResetCode)
+            .IsRequired()
+            .HasColumnName("PasswordRestCode")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(8);
     }
 }
---
+++ JwtStore.Infra/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using JwtStore.Core.Contexts.AccountContext.Entities;
+using JwtStore.Infra.Contexts.AccountContext.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtStore.Infra.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserMap());
+    }
+}
