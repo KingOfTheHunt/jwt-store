b795e78413a0eb030d3750cb1d0d670a2dd2d6ad
Davi <davifrancisco214@outlook.com>
2024-02-20 07:12:04 -0300

    Criando o banco de dados

---
+++ JwtStore.Api/Extensions/BuilderExtension.cs
@@ -0,0 +1,54 @@
+using System.Text;
+using JwtStore.Core.Contexts.AccountContext;
+using JwtStore.Infra.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtStore.Api.Extensions;
+
+public static class BuilderExtension
+{
+    public static void AddConfiguration(this WebApplicationBuilder builder)
+    {
+        Configuration.Database.ConnectionString = builder
+            .Configuration.GetConnectionString("Default") ?? string.Empty;
+        
+        Configuration.Secrets.ApiKey = builder
+            .Configuration.GetSection("Secrets").GetValue<string>("ApiKey") ?? string.Empty;
+        Configuration.Secrets.JwtKey = builder
+            .Configuration.GetSection("Secrets").GetValue<string>("JwtPrivateKey") ?? string.Empty;
+        Configuration.Secrets.PasswordSaltKey = builder.Configuration
+            .GetSection("Secrets").GetValue<string>("PasswordSaltKey") ?? string.Empty;
+    }
+
+    public static void AddDatabase(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(options => 
+        {
+            options.UseSqlServer(Configuration.Database.ConnectionString, 
+                x => x.MigrationsAssembly("JwtStore.Api"));
+        });
+    }
+
+    public static void AddJwtAuthentication(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(options => 
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options => 
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters 
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.Secrets.JwtKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+
+        builder.Services.AddAuthorization();
+    }
+}
---
+++ JwtStore.Api/Migrations/20240220100543_v1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JwtStore.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(120)", maxLength: 120, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(150)", maxLength: 150, nullable: false),
+                    EmailVerificationCode = table.Column<string>(type: "VARCHAR(6)", maxLength: 6, nullable: false),
+                    EmailVerificationExpiresAt = table.Column<TimeOnly>(type: "TIME", nullable: true),
+                    EmailVerificationVerifiedAt = table.Column<TimeOnly>(type: "TIME", nullable: true),
+                    PasswordHash = table.Column<string>(type: "NVARCHAR", nullable: false),
+                    PasswordRestCode = table.Column<string>(type: "VARCHAR(8)", maxLength: 8, nullable: false),
+                    Image = table.Column<string>(type: "VARCHAR(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
