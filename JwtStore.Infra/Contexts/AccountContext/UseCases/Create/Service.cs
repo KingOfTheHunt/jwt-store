0f84350f1b51e5a3c06b2c13d9ae45fc204961ff
Davi <davifrancisco214@outlook.com>
2024-02-22 07:51:09 -0300

    Registrando as dependencias

--- JwtStore.Infra/Contexts/AccountContext/UseCases/Create/Service.cs
+++ JwtStore.Infra/Contexts/AccountContext/UseCases/Create/Service.cs
@@ -4,19 +4,18 @@ using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
-namespace JwtStore.Infra.Contexts.AccountContext.UseCases.Create
+namespace JwtStore.Infra.Contexts.AccountContext.UseCases.Create;
+
+public class Service : IService
 {
-    public class Service : IService
+    public async Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
     {
-        public async Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
-        {
-            var client = new SendGridClient(Configuration.SendGrid.ApiKey);
-            var from = new EmailAddress(Configuration.Email.DefaultEmail, Configuration.Email.DefaultSenderName);
-            var subject = "Verifique a sua conta";
-            var to = new EmailAddress(user.Email, user.Name);
-            var content = $"Código de ativação: {user.Email.Verification.Code}";
-            var message = MailHelper.CreateSingleEmail(from, to, subject, content, content);
-            await client.SendEmailAsync(message, cancellationToken);
-        }
+        var client = new SendGridClient(Configuration.SendGrid.ApiKey);
+        var from = new EmailAddress(Configuration.Email.DefaultEmail, Configuration.Email.DefaultSenderName);
+        var subject = "Verifique a sua conta";
+        var to = new EmailAddress(user.Email, user.Name);
+        var content = $"Código de ativação: {user.Email.Verification.Code}";
+        var message = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+        await client.SendEmailAsync(message, cancellationToken);
     }
 }
